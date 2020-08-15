a053b12153abf62b3a720e58b8ef9243618156f0
Saiful Islam <67482122+Saifulctg@users.noreply.github.com>
2020-08-15 21:09:20 +0600

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,47 @@
-$(function(){
+$(function () {
     'use strict'
 
     // Sticky Menu Code Start
     var headerOffset = $('#header').offset().top
 
-    $(window).on('scroll', function(){
-       var scrollSize = $(window).scrollTop()
-   
-       if(scrollSize > headerOffset){
+    $(window).on('scroll', function () {
+        var scrollSize = $(window).scrollTop()
+
+        if (scrollSize > headerOffset) {
             $('#header').addClass('headerFixed')
-       } else{
-           $('#header').removeClass('headerFixed')
-       }
+        } else {
+            $('#header').removeClass('headerFixed')
+        }
     })
-   
+
     // Sticky Menu Code End
 
     // Banner Slide Code Start
     $('.bannerSlide').slick({
-        arrows:false,
-        autoplay:true,
-        autoplaySpeed:2000,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
         infinite: true,
-        speed:1000,
-        dots:true,
-        dotsClass:'bannerDots',
+        speed: 1000,
+        dots: true,
+        dotsClass: 'bannerDots',
     });
     // Banner Slide Code End
+
+    // ==========Back To Top button code start==========
+
+    $(window).on('scroll', function () {
+        var scrollSize = $(window).scrollTop()
+
+        if (scrollSize > 800) {
+            $('.backToTop').fadeIn(500)
+        } else {
+            $('.backToTop').fadeOut(500)
+        }
+    })
+    $('.backToTop').on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, '1000');
+    })
 })
